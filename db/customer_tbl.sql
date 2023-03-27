36fffb6ade31aa2bd7022961438c01f367d7dcba
Kweng1 <isidorokyle123@gmail.com>
2023-03-27 09:41:17 +0800

    finish database

--- db/customer_tbl.sql
+++ db/customer_tbl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2023 at 04:09 PM
+-- Generation Time: Mar 27, 2023 at 03:38 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -32,6 +32,9 @@ CREATE TABLE `customer_tbl` (
   `c_name` varchar(50) NOT NULL,
   `c_add` varchar(50) NOT NULL,
   `c_order` varchar(20) NOT NULL,
+  `c_size` varchar(50) NOT NULL,
+  `c_quant` int(50) NOT NULL,
+  `c_price` int(50) NOT NULL,
   `c_tp` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -39,8 +42,11 @@ CREATE TABLE `customer_tbl` (
 -- Dumping data for table `customer_tbl`
 --
 
-INSERT INTO `customer_tbl` (`c_id`, `c_name`, `c_add`, `c_order`, `c_tp`) VALUES
-(5, 'wqe', 'qweqwe', 'qweqwe', 11111);
+INSERT INTO `customer_tbl` (`c_id`, `c_name`, `c_add`, `c_order`, `c_size`, `c_quant`, `c_price`, `c_tp`) VALUES
+(8, 'Kyle', 'Talisay', 'Hawaiian', 'small', 2, 200, 400),
+(9, 'Aeron Villafuerte', 'Argao', 'Italian Pizza', 'Small', 1, 120, 120),
+(10, 'Caryll', 'Sibonga', 'Hawaiian', 'Medium', 2, 150, 300),
+(12, 'aa', 'aa', 'aaa', 'aaaa', 111, 1111, 123321);
 
 --
 -- Indexes for dumped tables
@@ -60,7 +66,7 @@ ALTER TABLE `customer_tbl`
 -- AUTO_INCREMENT for table `customer_tbl`
 --
 ALTER TABLE `customer_tbl`
-  MODIFY `c_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `c_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
